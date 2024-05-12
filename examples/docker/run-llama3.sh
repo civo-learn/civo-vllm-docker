7605cf2fd8cc2e715bff0534b2d3a962f50f8a3a
Josh <josh@civo.com>
2024-05-12 01:51:18 +0100

    added boilerplate for docker / run local example

---
+++ examples/docker/run-llama3.sh
@@ -0,0 +1,5 @@
+# TODO: make this example via docker e.g
+# IMAGE_NAME=ghcr.io/civo-learn/civo-vllm-docker:latest
+# do something
+# docker run -e HF_TOKEN=$HF_TOKEN -e MODEL=$MODEL $IMAGE_NAME -p 8080:8080
+# curl -X POST "http://localhost:8080/llama3" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"text\":\"I am a sentence and I am a sentence too\"}" 
